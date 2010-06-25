1451ecafd3b1725c9cc7f8b18b840eef0c7fce3c
Giovanni <potomak84@gmail.com>
2010-06-25 09:53:18 +0200

    add attachments support to send_message and reply_message methods

--- lib/has_messages.rb
+++ lib/has_messages.rb
@@ -35,17 +35,27 @@ module Xinuc
         Message.read_message self, message_id
       end
 
-      def send_message(receiver, subject, body)
-        @message = Message.new(:receiver => receiver, :sender => self, :subject => subject,
-          :body => body)
+      def send_message(receiver, subject, body, attachment = nil)
+        @message = Message.new(
+          :receiver   => receiver,
+          :sender     => self,
+          :subject    => subject,
+          :body       => body,
+          :attachment => attachment)
         @message.save
-        @message.deliver_message_notification! if (defined?(Notifier) and Notifier.respond_to?(:message_notification))
+        @message.deliver_message_notification! if (defined?(MessageMailer) and MessageMailer.respond_to?(:message_notification))
       end
 
-      def reply_message(message, subject, body)
-        @message = Message.new(:receiver => message.sender, :sender => self, :subject => subject, :body => body, :reply_of => message.id)
+      def reply_message(message, subject, body, attachment = nil)
+        @message = Message.new(
+          :receiver => message.sender,
+          :sender => self,
+          :subject => subject,
+          :body => body,
+          :attachment => attachment,
+          :reply_of => message.id)
         @message.save
-        @message.deliver_message_notification! if (defined?(Notifier) and Notifier.respond_to?(:message_notification))
+        @message.deliver_message_notification! if (defined?(MessageMailer) and MessageMailer.respond_to?(:message_notification))
       end
 
       def delete_message(message_id)
